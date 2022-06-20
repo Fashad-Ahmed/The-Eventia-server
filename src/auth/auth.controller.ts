f0e334776ad50a48d4e93b531c3a0315904b23f9
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2022-06-20 11:24:37 +0500

    Auth module completed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/signin')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ): Promise<any> {
+    try {
+      const res = await this.authService.signin(email, password);
+      return res;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  @Post('/signup')
+  async saveUser(
+    @Body('userName') userName: any,
+    @Body('email') email: any,
+    @Body('password') password: any,
+  ): Promise<any> {
+    return await this.authService.saveUser(userName, email, password);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  HttpStatus,
+  HttpException,
+  ConflictException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Auth } from './auth.model';
+const bcrypt = require('bcryptjs');
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  products: Auth[] = [];
+  constructor(@InjectModel('Auth') private readonly authModel: Model<Auth>) {}
+
+  async signin(email: any, password: any) {
+    console.log('sign in');
+    try {
+      try {
+        const userExist = await this.authModel.findOne({ email: email });
+        if (!userExist) {
+          throw new NotFoundException('User does not exist');
+        }
+
+        console.log(bcrypt.compareSync(password, userExist.password));
+        console.log(!bcrypt.compareSync(password, userExist.password));
+
+        if (!bcrypt.compareSync(password, userExist.password)) {
+          console.log('Wrong password');
+          throw new NotFoundException('Wrong Password');
+        }
+
+        const user = {
+          userExist,
+        };
+        console.log(user);
+        return { ...user, statusCode: 200 };
+      } catch (error) {
+        throw [404, error.message];
+      }
+    } catch (error) {
+      console.log(error);
+      throw [404, error.message];
+    }
+  }
+
+  async saveUser(userName: any, email: any, password: any): Promise<any> {
+    try {
+      console.log('save admin new user', userName, email, password);
+      const uniqueMail = await this.authModel.findOne({ email: email });
+      console.log(uniqueMail);
+      if (!uniqueMail) {
+        console.log('save admin new user', userName, email, password);
+
+        const bcryptPass = bcrypt.hashSync(password, 8);
+        console.log('bcrypt pass', bcryptPass);
+
+        const newAdminUser = new this.authModel({
+          userName,
+          email,
+          password: bcryptPass,
+        });
+
+        const result = await newAdminUser.save();
+        console.log(result);
+        return result;
+      } else {
+        throw new ConflictException('User already exist');
+      }
+    } catch (error) {
+      console.log('error', error);
+      throw [404, 'something went wrong'];
+    }
+  }
+}

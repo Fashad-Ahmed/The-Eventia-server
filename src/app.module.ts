e3b46eabb7f7bd1061dac25cfbc83982ae8e9014
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2022-06-20 13:47:26 +0500

    DB Connection | possible modules created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+import config from './config/keys';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService, AuthService],
+  imports: [MongooseModule.forRoot(config.mongoURI), AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/config/keys.ts
@@ -0,0 +1,6 @@
+const config = {
+  mongoURI:
+    'mongodb+srv://user:user@cluster0.4g9lxjq.mongodb.net/?retryWrites=true&w=majority',
+};
+
+export default config;

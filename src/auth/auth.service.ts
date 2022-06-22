565932f3c65492aa45e6a26efb81b6772823cc33
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2022-06-22 19:32:11 +0500

    cleanup 🔮

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,6 +8,7 @@ import {
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Auth } from './auth.model';
+import * as nodemailer from 'nodemailer';
 const bcrypt = require('bcryptjs');
 
 @Injectable()

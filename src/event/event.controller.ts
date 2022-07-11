a6e3c59736200124f68bfb4705973dc1fedbef27
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2022-07-12 02:48:56 +0500

    event controller and services completed

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
+import { response } from 'express';
+import { Date } from 'mongoose';
+import { Event } from './event.model';
+import { EventService } from './event.service';
 @Controller('event')
-export class EventController {}
+export class EventController {
+  constructor(private readonly eventService: EventService) {}
+
+  @Post('/createEvent')
+  async createEvent(@Res() response: Response): Promise<any> {
+    const newEvent = await this.eventService.create(response.body);
+    return {
+      newEvent,
+    };
+  }
+
+  @Get('/fetchEvent')
+  async fetchEvent(@Res() response): Promise<any> {
+    const admins = await this.eventService.fetchAll();
+    return {
+      admins,
+    };
+  }
+
+  @Put('/:id')
+  async update(@Res() response, @Param('id') id): Promise<any> {
+    const newEvent = await this.eventService.update(response, id);
+    return {
+      newEvent,
+    };
+  }
+
+  @Delete('/:id')
+  async delete(@Res() response, @Param('id') id) {
+    const deletedAdmin = await this.eventService.delete(id);
+    return response.status(HttpStatus.OK).json({
+      deletedAdmin,
+    });
+  }
+}
--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,4 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Event } from './event.model';
+import { Auth } from '../auth/auth.model';
 
 @Injectable()
-export class EventService {}
+export class EventService {
+  products: Event[] = [];
+  constructor(
+    @InjectModel('Event') private readonly eventModel: Model<Event>,
+    @InjectModel('Auth') private readonly authModel: Model<Auth>,
+  ) {}
+
+  async create(req): Promise<any> {
+    console.log('event create request started', req);
+    try {
+      const newEvent = await new this.eventModel({
+        name: req.name,
+        createdAt: Date.now(),
+        description: req.description,
+        category: req.category,
+        userId: req.userId,
+      });
+
+      const event = await this.eventModel.create(newEvent);
+      return { ...event, statusCode: 200 };
+    } catch (error) {
+      console.log(error);
+      throw [404, error.message];
+    }
+  }
+
+  async fetchAll(): Promise<any> {
+    console.log('event get request started');
+
+    try {
+      const getEvent = await this.eventModel.find().exec();
+      if (!getEvent) {
+        console.log(`no event found!`);
+      }
+      return getEvent;
+    } catch (error) {
+      console.log(error);
+      throw [404, error.message];
+    }
+  }
+
+  async update(req, id): Promise<any> {
+    console.log('req', req);
+    console.log('id', id);
+
+    try {
+      const newEvent = await new this.eventModel({
+        name: req.name,
+        createdAt: Date.now(),
+        description: req.description,
+        category: req.category,
+        userId: req.userId,
+      });
+
+      const event = await this.eventModel.findByIdAndUpdate(id, newEvent, {
+        new: true,
+      });
+      return { ...event, statusCode: 200 };
+    } catch (error) {
+      console.log(error);
+      throw [404, error.message];
+    }
+  }
+
+  async delete(id): Promise<any> {
+    console.log('id', id);
+
+    try {
+      return await this.eventModel.findByIdAndRemove(id);
+    } catch (error) {
+      console.log(error);
+      throw [404, error.message];
+    }
+  }
+}

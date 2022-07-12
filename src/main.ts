c759cd1bdd04c2529bc7a06a6bafd5a54e487c61
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2022-07-13 00:36:52 +0500

    Swagger API Documentation added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('The Eventia API Documentation ')
+    .setDescription('@FashadAhmed')
+    .setVersion('v-1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('apiDocs', app, document);
   await app.listen(3002);
 }
 bootstrap();
